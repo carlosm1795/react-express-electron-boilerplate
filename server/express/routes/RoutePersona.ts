4dc303a10805b08d18a99b0b730d00f230357dcd
Carlos <carlosm1795@gmail.com>
2022-06-16 18:56:15 -0600

    Adding Entries

--- server/express/routes/RoutePersona.ts
+++ server/express/routes/RoutePersona.ts
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 import { Request, Response } from 'express';
-import {getPersonas} from "../DA/Personas";
+import {getPersonas,deletePersona,getAgeByIdPersona,getPersonasById,newPersona,updatePersonas,CalculateTimeFromNombreAstral} from "../DA/Personas";
 
 router.get("/getPersonas",async(req:Request,res:Response) => {
     try {
@@ -11,6 +11,57 @@ router.get("/getPersonas",async(req:Request,res:Response) => {
         res.send({ error: elError });
     }
 })
+
+router.post("/deletePersona", async (req:Request,res:Response) => {
+    try {
+        const results = await deletePersona(req);
+        res.send(JSON.stringify(results));
+    } catch (elError) {
+        res.send({ error: elError });
+    }
+});
+
+router.post("/getAgeByIdPersona", async (req:Request,res:Response) => {
+    try {
+        console.log("aqui")
+        const results = await getAgeByIdPersona(req);
+        res.send(JSON.stringify(results));
+    } catch (elError) {
+        res.send({ error: elError });
+    }
+});
+
+router.post("/getPersonasById", async (req:Request,res:Response) => {
+    
+    try {
+        const results = await getPersonasById(req);
+        res.send(JSON.stringify(results));
+    } catch (elError) {
+        res.send({ error: elError });
+    }
+});
+
+router.post("/newPersona", async (req:Request,res:Response) => {
+    
+    try {
+        const results = await newPersona(req);
+
+        res.send(JSON.stringify(results));
+    } catch (elError) {
+        res.send({ error: elError });
+    }
+});
+
+router.post("/updatePersona", async (req:Request,res:Response) => {
+    
+    try {
+        const results = await updatePersonas(req);
+
+        res.send(JSON.stringify(results));
+    } catch (elError) {
+        res.send({ error: elError });
+    }
+});
 
 module.exports = router;
 
